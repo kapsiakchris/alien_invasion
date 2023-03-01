1bee04ffd566cbaa3096628cb824fd41d1320a2b
Chris Kapsiak <kapsiakchris@gmail.com>
2023-03-01 10:48:07 -0500

    added ship movement

---
+++ ship.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set starting position"""
+
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+    def blitme(self):
+        """draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
